a9ee6b157d9388dd2462c08e8a7d8e54afec834e
hidehigo <hidehigo@gmail.com>
2012-10-17 20:03:55 +0900

    added migrate005

--- resevation/db/migrate/005_modify_timebox_columns.rb
+++ resevation/db/migrate/005_modify_timebox_columns.rb
@@ -1,11 +1,11 @@
 class ModifyTimeboxColumns < ActiveRecord::Migration
   def self.up
-    change_table :timeboxes do |t|
-      #t.
-    end
-
+    change_column( :timeboxes, :start, :time )
+    change_column( :timeboxes, :end  , :time )
   end
 
   def self.down
+    change_column( :timeboxes, :start, :datetime )
+    change_column( :timeboxes, :end  , :datetime )
   end
 end
--- resevation/db/schema.rb
+++ resevation/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 4) do
+ActiveRecord::Schema.define(:version => 5) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(:version => 4) do
   end
 
   create_table "timeboxes", :force => true do |t|
-    t.datetime "start"
-    t.datetime "end"
+    t.time     "start"
+    t.time     "end"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "shop_id"
